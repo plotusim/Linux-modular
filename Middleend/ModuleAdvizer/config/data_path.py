9ed9311420e1eb41c19f042622f77167e1638752
PLotusim <1301231127@qq.com>
2023-09-18 19:38:18 +0800

    Fix path script

--- Middleend/ModuleAdvizer/config/data_path.py
+++ Middleend/ModuleAdvizer/config/data_path.py
@@ -1,9 +1,19 @@
-file_types_folder = './data/file_labels/'
-init_func_list_file = './data/initfuncs.txt'
-init_reach_func_list_file = './data/init_reach_funcs.txt'
-export_func_list_file = './data/export_symbols.txt'
-whole_kernel_dot_file = './data/new_linux_all_with_file_label.dot'
-dots_root_folder = './data/dots/new_dots_suffix'
-whole_kernel_graph_cache = './data/whole_kernel_graph_cache.pkl'
-virtual_structs_reach_level1_list_file = './data/virtual_structs_reach_level1_list_file.txt'
-all_virtual_structs_reach_funcs_list_file = './data/all_virtual_structs_reach_funcs_list_file.txt'
+import os
+
+current_path = os.path.abspath(__file__)
+project_root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(current_path))))
+data_folder = os.path.join(project_root_path, "Data")
+
+dots_root_folder = os.path.join(data_folder, 'dots')
+
+init_func_list_file = os.path.join(data_folder, 'func_list', 'initfuncs.txt')
+init_reach_func_list_file = os.path.join(data_folder, 'func_list', 'init_reach_funcs.txt')
+export_func_list_file = os.path.join(data_folder, 'func_list', 'export_symbols.txt')
+virtual_structs_reach_level1_list_file = os.path.join(data_folder, 'func_list',
+                                                      'virtual_structs_reach_level1_list_file.txt')
+all_virtual_structs_reach_funcs_list_file = os.path.join(data_folder, 'func_list',
+                                                         'all_virtual_structs_reach_funcs_list_file.txt')
+
+whole_kernel_dot_file = os.path.join(data_folder, 'new_linux_all_with_file_label.dot')
+
+whole_kernel_graph_cache = os.path.join(data_folder, 'whole_kernel_graph_cache.pkl')
