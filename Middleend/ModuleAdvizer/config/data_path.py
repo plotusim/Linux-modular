b166345f313ef6cb28af85b65fed4aff873ade41
PLotusim <1301231127@qq.com>
2023-09-19 10:45:05 +0800

    Add .gitignore

--- Middleend/ModuleAdvizer/config/data_path.py
+++ Middleend/ModuleAdvizer/config/data_path.py
@@ -4,9 +4,13 @@ current_path = os.path.abspath(__file__)
 project_root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(current_path))))
 data_folder = os.path.join(project_root_path, "Data")
 
+# 存放前端生成的dots
 dots_root_folder = os.path.join(data_folder, 'dots')
 
+# 存放前端分析的init函数列表
 init_func_list_file = os.path.join(data_folder, 'func_list', 'initfuncs.txt')
+
+
 init_reach_func_list_file = os.path.join(data_folder, 'func_list', 'init_reach_funcs.txt')
 export_func_list_file = os.path.join(data_folder, 'func_list', 'export_symbols.txt')
 virtual_structs_reach_level1_list_file = os.path.join(data_folder, 'func_list',
