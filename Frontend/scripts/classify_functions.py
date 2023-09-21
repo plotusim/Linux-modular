49cdb41e6d3ce93504e17bdba0fc1bd06c0bd5c5
PLotusim <1301231127@qq.com>
2023-09-21 10:47:34 +0800

    Classify modualr functions

--- Frontend/scripts/classify_functions.py
+++ Frontend/scripts/classify_functions.py
@@ -1,16 +1,20 @@
 from elftools.elf.elffile import ELFFile
+import pydot
+from simple_parse_makefile import parseconfig
 
-
-
-vmlinux_path = "../kernel_src/vmlinux"
-## This scripts used for classfifying kernel functions
+## This scripts used for classifying kernel functions
 
 init_funcs = set()
 export_funcs = set()
 
-init_reach_funcs= set()
+init_reach_funcs = set()
 virtual_structs = set()
 virtual_structs_reach = set()
+
+module_funcs = set()
+
+# 存储边
+edge_dict = {}
 
 # 识别init函数,export函数
 def find_symbols_from_vmlinux(vmlinux_path):
@@ -64,10 +68,73 @@ def find_symbols_from_vmlinux(vmlinux_path):
 
 #识别init_reach函数，识别virtual_structs函数，识别virtual_struts_reach函数
 def find_symbols_from_kernel_dot(kernel_dot):
+    print("read kernel dot")
+    graph = pydot.graph_from_dot_file(kernel_dot)[0]
+    print("read kernel dot success")
+    
+    # 识别virtual_structs函数
+    nodes = graph.get_nodes()
+    for node in nodes:
+        name = node.get_name()
+        if name.endswith("__virtual_init"):
+            virtual_structs.add(name)
+    print("virtual structs :" + str(len(virtual_structs)))
 
-            
 
+    edges = graph.get_edges()
+    for edge in edges:
+        src = edge.get_source()
+        des = edge.get_destination() 
+        edge_dict.setdefault(src,set()).add(des)
+
+    # 识别virtual_structs_reach函数
+    find = set()
+    for i in virtual_structs:
+        if i in edge_dict.keys():
+            for j in edge_dict[i]:
+                if j not in virtual_structs and j not in find:
+                    find.add(j)
+
+    while(len(find)>0):
+        element = find.pop()
+        virtual_structs_reach.add(element)
+        if element in edge_dict.keys():
+            for child in edge_dict[element]:
+                if child not in virtual_structs_reach:
+                    find.add(child)
+    print("virtual structs reach :" + str(len(virtual_structs_reach)))
+    
+    #识别init_reach函数
+    init_find = set()
+    for i in init_funcs:
+        if i in edge_dict.keys():
+            for j in edge_dict[i]:
+                if j not in init_funcs and j not in init_find:
+                    init_find.add(j)
+
+    while(len(init_find)>0):
+        element = init_find.pop()
+        init_reach_funcs.add(element)
+        if element in edge_dict.keys():
+            for child in edge_dict[element]:
+                if child not in init_reach_funcs:
+                    init_find.add(child)
+    print("init reach funcs :" + str(len(init_reach_funcs)))
+
+
+
+# 识别已模块化函数
+def find_modular_functions():
+    find_module_files = set()
+    tempConfig = parseconfig(kernel_path="/home/plot/linux-5.10.176",config=".config")
+    find_module_files = tempConfig.modualr_files
+    print(find_module_files)
 
 if __name__ == "__main__":
-    vmlinux_path_test = "/home/plot/linux-5.10.176/vmlinux"
-    find_symbols_from_vmlinux(vmlinux_path_test)
+    # vmlinux_path_test = "/home/plot/linux-5.10.176/vmlinux"
+    # find_symbols_from_vmlinux(vmlinux_path_test)
+    
+    # kernel_dot = "/home/plot/hn_working_dir/Linux-modular/Graph/data/new_linux_all_with_file_label.dot"
+    # find_symbols_from_kernel_dot(kernel_dot)
+    
+    find_modular_functions()
---
+++ Frontend/scripts/simple_parse_makefile.py
@@ -0,0 +1,319 @@
+import re
+from kconfiglib import *
+import os
+import copy
+from difflib import SequenceMatcher
+from openpyxl import Workbook
+
+'''
+数据结构说明:
+文件节点:file_node
+CONFIG节点
+configs[CONFIG] = {file_node}
+CONFIG依赖图 graph (使用kconfiglib)
+'''
+
+subsystem = ["block","certs","crypto","drivers","fs","init","io_uring","ipc","kernel","lib","mm","security"\
+    ,"sound","usr","virt","net"]
+
+class myconfig:    
+    def __init__(self,name):
+        self.config_name = name
+        self.config_parents = set()
+        self.config_childrens = set()
+        self.files = []
+        self.compile_files = []
+
+
+class parseconfig:
+
+    kconf = None
+
+    # 用于解析makefile语句的模式
+    # obj-$(CONFIG_xxx) (:|+|)= yyy
+    pattern1 = re.compile(r'^obj-\$\((\w+)\)\s*((:|\+|)=) \s*(.+)$')
+    # obj-y (:|+)= yyy
+    pattern2 = re.compile(r'^obj-y\s*((\+|:|)=) \s*(.+)$')
+    # xxx-(y|objs) (:|+)= yyy
+    pattern3 = re.compile(r'^([-_\w]+)-(y|objs)\s*((:|\+|)=)\s*(.+)$')
+    # xxx-$(CONFIG_xxx)(:|+)=yyy
+    pattern4 = re.compile(r'^([-_\w]+)-\$\((\w+)\)\s*((\+|:|)=) \s*(.+)$')
+    # xxx(:/+)= zzz
+    pattern5 = re.compile(r'^([-\w]+)\s*((\+|:|)=)\s*(.+)')
+    # xxx-$(subst m,y,$(CONFIG_xxx))	+= yyy
+    
+    # 解析makefile        
+    def parse_makefile(self,config,file_path):
+        temp_dict = {}
+        temp_object = {}
+        temp_dollar = {}
+        temp_compile_object = {}
+        temp_compile_dependcy = {}
+        temp_core = set()
+        try:
+            makefilepath = os.path.join(file_path,"Makefile")
+            with open(makefilepath, "r") as f:
+                lines = f.readlines()
+        except FileNotFoundError:
+            kbuildfilepath = os.path.join(file_path,"Kbuild")
+            with open(kbuildfilepath, "r") as f:
+                lines = f.readlines()
+        i = 0
+        while i < len(lines):
+            line = lines[i].strip()
+            match = self.pattern2.match(line)
+            if match:
+                target_list = match.group(3).split()
+                while "\\" in target_list:
+                    target_list.remove("\\") 
+                    i += 1
+                    if i >= len(lines):
+                        break
+                    line = lines[i].strip()
+                    # 解决“cfdbgl.o\”中文件与“\”没分开问题kconfiglib.KconfigError: init/Kconfig:29: error: couldn't parse 'def_bool $(success,echo "$(CC_VERSION_TEXT)
+                    temp_line = line.split()
+                    if len(temp_line) > 0 and "\\"!= temp_line[-1] and "\\" in temp_line[-1]:
+                        temp_file = temp_line[-1].split('\\')
+                        temp_line.remove(temp_line[-1])
+                        temp_line.append(temp_file[0])
+                        temp_line.append('\\')
+                    target_list += temp_line
+
+                if config == "":
+                    temp_core.update(target_list)
+                else:
+                    dict_value = temp_dict.setdefault(config, [])
+                    dict_value.extend(target_list)
+                i += 1
+                continue  
+            match = self.pattern1.match(line)
+            if match:
+                config_name = match.group(1)
+                target_list = match.group(4).split()
+                while "\\" in target_list:
+                    target_list.remove("\\") 
+                    i += 1
+                    if i >= len(lines):
+                        break
+                    line = lines[i].strip()
+                    # 解决“cfdbgl.o\”中文件与“\”没分开问题
+                    temp_line = line.split()
+                    if len(temp_line) > 0 and "\\"!= temp_line[-1] and "\\" in temp_line[-1]:
+                        temp_file = temp_line[-1].split('\\')
+                        temp_line.remove(temp_line[-1])
+                        temp_line.append(temp_file[0])
+                        temp_line.append('\\')
+                    target_list += temp_line
+                dict_value = temp_dict.setdefault(config_name, [])
+                dict_value.extend(target_list)
+                i += 1
+                continue  
+            match = self.pattern3.match(line)
+            if match:
+                object_name = str(match.group(1)) + ".o"
+                dollar_name = str(match.group(1)) +'-'+ str(match.group(2))
+                target_list = match.group(5).split()
+                while "\\" in target_list:
+                    target_list.remove("\\") 
+                    i += 1
+                    if i >= len(lines):
+                        break
+                    line = lines[i].strip()
+                    # 解决“cfdbgl.o\”中文件与“\”没分开问题
+                    temp_line = line.split()
+                    if len(temp_line) > 0 and "\\"!= temp_line[-1] and "\\" in temp_line[-1]:
+                        temp_file = temp_line[-1].split('\\')
+                        temp_line.remove(temp_line[-1])
+                        temp_line.append(temp_file[0])
+                        temp_line.append('\\')
+                    target_list += temp_line
+                    
+                object_value = temp_object.setdefault(object_name, [])
+                object_value.extend(target_list)
+                
+                dollar_value = temp_dollar.setdefault(dollar_name, [])
+                dollar_value.extend(target_list)
+                i += 1
+                continue  
+            match = self.pattern4.match(line)
+            if match: #当前忽略该模式下的config dependcy关系
+                # 忽略ccflags
+                if match.group(1) == "ccflags":
+                    i += 1
+                    continue
+                object_name = str(match.group(1)) + ".o"
+                config_name = match.group(2)
+                target_list = match.group(5).split()
+                while "\\" in target_list:
+                    target_list.remove("\\") 
+                    i += 1
+                    if i >= len(lines):
+                        break
+                    line = lines[i].strip()
+                    # 解决“cfdbgl.o\”中文件与“\”没分开问题
+                    temp_line = line.split()
+                    if len(temp_line) > 0 and "\\"!= temp_line[-1] and "\\" in temp_line[-1]:
+                        temp_file = temp_line[-1].split('\\')
+                        temp_line.remove(temp_line[-1])
+                        temp_line.append(temp_file[0])
+                        temp_line.append('\\')
+                    target_list += temp_line
+                dict_value = temp_dict.setdefault(config_name, [])
+                dict_value.extend(target_list)
+                
+                object_value = temp_object.setdefault(object_name, [])
+
+                temp_compile_object.setdefault(object_name, set()).add(config_name)
+
+                i += 1
+                continue 
+            match = self.pattern5.match(line)
+            if match:
+                dollar_name = match.group(1)
+                target_list = match.group(4).split()
+                while "\\" in target_list:
+                    target_list.remove("\\") 
+                    i += 1
+                    if i >= len(lines):
+                        break
+                    line = lines[i].strip()
+                    # 解决“cfdbgl.o\”中文件与“\”没分开问题
+                    temp_line = line.split()
+                    if len(temp_line) > 0 and "\\"!= temp_line[-1] and "\\" in temp_line[-1]:
+                        temp_file = temp_line[-1].split('\\')
+                        temp_line.remove(temp_line[-1])
+                        temp_line.append(temp_file[0])
+                        temp_line.append('\\')
+                    target_list += temp_line
+                dollar_value = temp_dollar.setdefault(dollar_name, [])
+                dollar_value.extend(target_list)
+                i += 1
+                continue
+            
+            i += 1
+        #在dict中替换object,dollar,并对xxx/进行迭代
+        for key,value in temp_dict.items():
+            for object,file in temp_object.items():
+                if object in value:
+                    value.remove(object) 
+                    value += file
+                    if object in temp_compile_object.keys():
+                        temp_compile_dependcy.setdefault(key, set()).update(temp_compile_object[object])
+                        self.compile_dependcy.setdefault(key, set()).update(temp_compile_object[object])
+                    
+            for dollar_name,file in temp_dollar.items():
+                keyvalue = "$(" + str(dollar_name) + ")"
+                if keyvalue in value:
+                    value.remove(keyvalue)
+                    value += file
+            delete_i = set()               
+            for i in value:
+                if i.endswith("/"):
+                    new_path = os.path.join(file_path, str(i))
+                    self.parse_makefile(key,new_path)
+                    delete_i.add(i)
+            for i in delete_i:
+                value.remove(i)
+        # 迭代temp_core
+        for object,file in temp_object.items():
+            if object in temp_core:
+                temp_core.remove(object) 
+                temp_core.update(file)
+        for dollar_name,file in temp_dollar.items():
+            keyvalue = "$(" + str(dollar_name) + ")"
+            if keyvalue in temp_core:
+                temp_core.remove(keyvalue)
+                temp_core.update(file)
+        delete_i = set()               
+        for i in temp_core:
+            if i.endswith("/"):
+                new_path = os.path.join(file_path, str(i))
+                self.parse_makefile("",new_path)
+                delete_i.add(i)
+        for i in delete_i:
+            temp_core.remove(i)
+
+        # 更新到全局的dict
+        for key, value in temp_dict.items():
+            abs_value = []
+            for i in value:
+                abs_file = os.path.join(file_path, str(i))
+                temp_abs_file = abs_file.replace(".o", ".c")
+                abs_value.append(temp_abs_file)
+            if key in self.configs.keys():
+                self.configs[key].files += abs_value
+            else:
+                newconfig = myconfig(name=key)
+                newconfig.files = abs_value
+                self.configs[key] = newconfig
+        
+        # 更新compile_dependcy
+        for key, value in temp_compile_dependcy.items():
+            abs_compile = []
+            for i in value:
+                for j in temp_dict[i]:
+                    abs_compile_file = str(file_path) + str(j)
+                    temp_abs_compile_file = abs_compile_file.replace(".o", ".c")
+                    abs_compile.append(temp_abs_compile_file)
+            self.configs[key].compile_files = self.configs[key].files + abs_compile
+        
+        # 更新core_file
+        for i in temp_core:
+            abs_core_file = str(file_path) + str(i)
+            temp_core_file = abs_core_file.replace(".o", ".c")
+            self.core_files.add(temp_core_file)
+    
+    # 用于debug, print出makefile中未匹配的var,
+    def __fileter_file(self):
+        # 过滤config中的$(var)
+        for _, value in self.configs.items():   
+            temp_files = value.files
+            value.files = [item for item in temp_files if "$" not in item]
+            temp_compile_files = value.compile_files
+            value.compile_files = [it for it in temp_compile_files if "$" not in it]
+        # 过滤core中的$(var)
+        for i in self.core_files:
+            if "$" in i:
+                temp_core = self.core_files
+                self.core_files = {it for it in temp_core if "$" not in it}
+                
+    def output_modualr_file(self,kernel_path,config):
+        mconfig = list()
+        
+        config_path = os.path.join(kernel_path, config)
+        with open(config_path,"r") as cfile:
+            for cf_line in cfile:
+                if "=m" in cf_line:
+                    mconfig.append(cf_line.split('=')[0])
+         
+        for mcfg in mconfig:
+            if mcfg in self.configs.keys():
+                mcfg_config = self.configs[mcfg]
+                if mcfg_config.compile_files != []:
+                    temp_modualr_files = [f for f in mcfg_config.compile_files if f not in self.core_files]
+                else:
+                    temp_modualr_files = [f for f in mcfg_config.files if f not in self.core_files]
+            self.modualr_files.update(temp_modualr_files)
+    
+
+    def __init__(self,kernel_path,config):
+        # config类
+        self.configs = {}
+        # compile 依赖
+        self.compile_dependcy = {}
+        
+        self.core_files = set()
+        self.modualr_files = set()
+
+        # makefile相关
+            # 对每个子系统进行分析
+        for i in subsystem:
+            filePath = os.path.join(kernel_path, str(i))
+            self.parse_makefile("",filePath)
+
+        self.__fileter_file()  
+        
+        self.output_modualr_file(kernel_path,config)
+        
+        
+
